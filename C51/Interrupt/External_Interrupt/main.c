004a880ff4cf17096002f6764cd6008ee5ee40f0
main <qizhenghao@foxmail.com>
2023-01-13 18:58:35 +0800

    更新注释

--- C51/Interrupt/External_Interrupt/main.c
+++ C51/Interrupt/External_Interrupt/main.c
@@ -1,19 +1,19 @@
 #include <regx52.h>
+//regx52.h中有更多定义好了P2_0可以直接使用
 void Exterinal_Interrupt_Init();
 void main()
 {
     Exterinal_Interrupt_Init();
-    while (1)
-        ;
+    while (1);
 }
-
+//外部中断设置
 void Exterinal_Interrupt_Init()
 {
     IT0 = 1; // 设置触发方式为下降沿触发
     EX0 = 1; // 开启外部中断0
     EA = 1;  // 开启总中断
 }
-
+//外部中断处理函数 中断号为0
 void Exterinal_Interrupt_Function() interrupt 0
 {
     P2_0 = !P2_0;
--- C51/Interrupt/Timer_Interrupt/main.c
+++ C51/Interrupt/Timer_Interrupt/main.c
@@ -1,25 +1,28 @@
 #include <regx52.h>
 unsigned int count = 0;
+
 void Timer_Interrput_Init();
+
 void main()
 {
     Timer_Interrput_Init();
-    while (1)
-        ;
+    while (1);
 }
-
+//定时器设置
 void Timer_Interrput_Init()
 {
-    TMOD = 0x01;
-    TH0 = 0xfa;
-    TL0 = 0x00;
-    TR0 = 1;
-    ET0 = 1;
-    EA = 1;
+    TMOD = 0x01;//设置定时器模式
+    TH0 = 0xfa; //设置定时器初始值高位
+    TL0 = 0x00;//设置定时器初始值低位
+    TR0 = 1;//启用定时器
+    ET0 = 1;//开启定时器
+    EA = 1;//开启总中断
 }
+//定时器中断处理函数 中断号1
 void Timer_Intrrupt() interrupt 1
 {
-    TH0 = 0xfa;
+
+    TH0 = 0xfa;//设置定时器初始值
     TL0 = 0x00;
     ++count;
     if (count % 1000 == 0)
